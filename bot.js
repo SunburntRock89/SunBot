9a210c0047c4967d7936465e0ed057f585c8e60e
StupidDan <danielbee912@gmail.com>
2017-10-19 20:49:24 +0100

    Downgraded discord,js and added ping command

--- bot.js
+++ bot.js
@@ -2,4 +2,15 @@ const Discord = require('discord.js');
 const settings = require('./config.json');
 const client = new Discord.Client();
 
+client.on('message', message => {
+	if (message.content.startsWith(settings.prefix + "ping")) {
+		const embed = new Discord.RichEmbed();
+		embed.setTitle("PoniJS")
+		.setDescription("Pong! :ping_pong:")
+		.setColor("#5491F2")
+		.setFooter("v0.01 ALPHA")
+		message.channel.sendEmbed(embed);
+	}
+})
+
 client.login(settings.token);
