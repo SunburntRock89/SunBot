5112d92068dc810ea13849e96300548804e6f383
Unknown <SunburntRock89@gmail.com>
2017-10-19 21:31:02 +0100

    SHIT HAPPENS
    WE HANDLE COMMANDS NOW

---
+++ bot.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const settings = require('./config.json');
+const client = new Discord.Client();
+
+client.on("message", async msg => {
+    if (msg.author.bot) return null;
+    if (!msg.content.startsWith(settings.prefix)) return null;
+    const cmd = msg.content.split(" ")[0].trim().toLowerCase().replace(settings.prefix, "");
+    const suffix = msg.content.split(" ").splice(1).join(" ").trim();
+    let cmdFile;
+    try {
+        cmdFile = require(`./Commands/${cmd}.js`);
+    } catch (err) {
+        return console.error(err);
+    }
+    if (cmdFile) {
+        return await cmdFile(client, msg, suffix);
+    }
+});
+
+client.on('ready', () => {
+    client.user.setGame("being developed by the best chef");
+    client.user.setStatus("dnd");
+    console.log("PoniJS [Dan] is online!");
+})
+
+client.login(settings.token);
