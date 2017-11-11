48423cf4f6ce66990333c2c3cdc4a3b2cb3221b8
Unknown <SunburntRock89@gmail.com>
2017-11-11 22:58:10 +0000

    Ban and Kick error

--- Commands/ban.js
+++ Commands/ban.js
@@ -1,23 +1,33 @@
+const settings = require('../config.json');
+
 module.exports = async (client, msg, suffix) => {
-    let reason = msg.content.split(/\s+/g).slice(2).join(" ");    
+    let reason = msg.content.split(/\s+/g).slice(2).join(" ");  
     if(msg.mentions.users.size !== 0) {
-        try {
+        msg.mentions.members.first().ban(reason).then(() => {
             msg.channel.send({
                 embed: {
                     color: 0x5491F2,
                     author: {
                         name: "SunBot"
                     },
-                    description: `${msg.mentions.users.first().tag} has been banned`,
+                    description: `${msg.mentions.users.first().tag} has been banned.`,
                     footer: {
                         text: `You now have ${msg.guild.memberCount} members.`
                     },
                 },
             })
-            msg.mentions.members.first().ban(reason)
-        } catch (err) {
-            console.log(err)
-        }
+        }).catch(err => {
+            msg.channel.send({
+                embed: {
+                    color: 0xFF0000,
+                    title: ":x: Error",
+                    description: err.message,
+                    footer: {
+                        text: settings.version
+                    }
+                }
+            })
+        })
     } else {
         //msg.channel.guild.ban()
         msg.reply("You need to mention the user you want to ban or mention them via their ID.")
--- Commands/kick.js
+++ Commands/kick.js
@@ -1,31 +1,32 @@
+const settings = require('../config.json');
+
 module.exports = async (client, msg, suffix) => {
     let reason = msg.content.split(/\s+/g).slice(2).join(" ");  
     if(msg.mentions.users.size !== 0) {
-        try {
-            msg.mentions.members.first().kick(reason)
-        } catch (err) {
+        msg.mentions.members.first().kick(reason).then(() => {
+            msg.channel.send({
+                embed: {
+                    color: 0x5491F2,
+                    author: {
+                        name: "SunBot"
+                    },
+                    description: `${msg.mentions.users.first().tag} has been kicked.`,
+                    footer: {
+                        text: `You now have ${msg.guild.memberCount} members.`
+                    },
+                },
+            })
+        }).catch(err => {
             msg.channel.send({
                 embed: {
                     color: 0xFF0000,
                     title: ":x: Error",
-                    description: err,
+                    description: err.message,
                     footer: {
                         text: settings.version
                     }
                 }
-            });
-        }
-        msg.channel.send({
-            embed: {
-                color: 0x5491F2,
-                author: {
-                    name: "SunBot"
-                },
-                description: `${msg.mentions.users.first().tag} has been kicked.`,
-                footer: {
-                    text: `You now have ${msg.guild.memberCount} members.`
-                },
-            },
+            })
         })
     } else {
         //msg.channel.guild.ban()
